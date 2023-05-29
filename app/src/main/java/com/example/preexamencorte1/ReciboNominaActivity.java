3c2d491065ae263be699a3eb13dfc3a1d21438aa
BryanUziii <bryan.quintana2016@gmail.com>
2023-05-29 16:08:48 -0700

    Creacion segunda vista, y codigo de la principal

---
+++ app/src/main/java/com/example/preexamencorte1/ReciboNominaActivity.java
@@ -0,0 +1,21 @@
+package com.example.preexamencorte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ReciboNominaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibo_nomina);
+    }
+}
